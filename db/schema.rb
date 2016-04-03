d164c6141f8fb6206d6c775fba42ab5901ba54d0
AdStyle Marketing <doug@adstylemarketing.com>
2016-04-03 07:56:26 -0500

    tracing errors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160403041432) do
+ActiveRecord::Schema.define(version: 20160403044838) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,8 +42,9 @@ ActiveRecord::Schema.define(version: 20160403041432) do
     t.integer  "user_id"
     t.integer  "place_id"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.integer  "score",      default: 0
   end
 
   create_table "users", force: :cascade do |t|
